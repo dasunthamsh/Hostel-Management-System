bcde358b2057c3aa96ccaf3b94bf4b1486eb1134
dasunthamsh <tunathamash@gmail.com>
2023-03-27 23:56:50 +0530

    added

--- src/lk/ijse/hostelManagementSystem/util/FactoryConfiguration.java
+++ src/lk/ijse/hostelManagementSystem/util/FactoryConfiguration.java
@@ -2,5 +2,28 @@ package lk.ijse.hostelManagementSystem.util;/*
     @author Dasun
 */
 
+import com.sun.deploy.security.SessionCertStore;
+import lk.ijse.hostelManagementSystem.Controller.DashboardFormController;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+
+    private static FactoryConfiguration factoryConfiguration;
+    private  SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        sessionFactory = new Configuration().mergeProperties(Utility.getProperties())
+                .addAnnotatedClass(DashboardFormController.class)
+                .buildSessionFactory();
+    }
+
+    public FactoryConfiguration getInstance(){
+        return (factoryConfiguration == null) ? new FactoryConfiguration() : factoryConfiguration;
+    }
+        public Session getSessino(){
+            return sessionFactory.openSession();
+        }
+
 }
--- src/lk/ijse/hostelManagementSystem/util/Utility.java
+++ src/lk/ijse/hostelManagementSystem/util/Utility.java
@@ -2,5 +2,19 @@ package lk.ijse.hostelManagementSystem.util;/*
     @author Dasun
 */
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Utility {
+
+    public static Properties getProperties(){
+        Properties properties = new Properties();
+
+        try {
+            properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
 }
