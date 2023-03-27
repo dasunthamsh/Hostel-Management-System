8add873380b655d138d983c21009c38de1eda24e
dasunthamsh <tunathamash@gmail.com>
2023-03-28 04:18:19 +0530

    added

--- src/lk/ijse/hostelManagementSystem/Controller/LoginFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/LoginFormController.java
@@ -2,5 +2,24 @@ package lk.ijse.hostelManagementSystem.Controller;/*
     @author Dasun
 */
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+    public JFXButton btnSignIn;
+    public JFXButton btnCreateAccount;
+    public JFXButton btnForgetPassword;
+
+    public void btnSignOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCreateAccountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnForgetPasswordOnAction(ActionEvent actionEvent) {
+    }
 }
