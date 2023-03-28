d545a32b77489d92a977d6a5a56bb9a3bd5aba15
dasunthamsh <tunathamash@gmail.com>
2023-03-28 20:57:57 +0530

    added

--- src/lk/ijse/hostelManagementSystem/util/Navigation.java
+++ src/lk/ijse/hostelManagementSystem/util/Navigation.java
@@ -2,5 +2,22 @@ package lk.ijse.hostelManagementSystem.util;/*
     @author Dasun
 */
 
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class Navigation {
+
+    public static AnchorPane pane;
+    public static void navigate(Routes routes , Anchorpane pane){
+
+        Navigation.pane=pane;
+        Navigation.pane.getChildren().clear();
+        Stage window = (Stage) Navigation.pane.getScene().getWindow();
+
+
+        switch (routes){
+            case Loging
+        }
+    }
+
 }
