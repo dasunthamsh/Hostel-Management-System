75ca917e0be40e170e245917489767116054d4e0
dasunthamsh <tunathamash@gmail.com>
2023-03-30 02:06:10 +0530

    added

--- src/lk/ijse/hostelManagementSystem/Controller/ReserveRoomFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/ReserveRoomFormController.java
@@ -2,5 +2,24 @@ package lk.ijse.hostelManagementSystem.Controller;/*
     @author Dasun
 */
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class ReserveRoomFormController {
+    public JFXComboBox cmbStudentId;
+    public JFXComboBox cmbRoomId;
+    public JFXButton btnReservRoom;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtroomType;
+    public JFXTextField txtAvalableQyt;
+    public JFXTextField txtMoney;
+    public JFXComboBox cmbPayment;
+    public Label lblTime;
+    public Label lblReserveId;
+
+    public void btnReservRoomOnAction(ActionEvent actionEvent) {
+    }
 }
