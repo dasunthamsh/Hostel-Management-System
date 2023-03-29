378f3404e17a31e0841c9baa12e4d57f8bbcd7a0
dasunthamsh <tunathamash@gmail.com>
2023-03-30 01:48:04 +0530

    added

--- src/lk/ijse/hostelManagementSystem/Controller/ManageRoomFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/ManageRoomFormController.java
@@ -2,5 +2,29 @@ package lk.ijse.hostelManagementSystem.Controller;/*
     @author Dasun
 */
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class ManageRoomFormController {
+    public JFXTextField btnKeyMoney;
+    public JFXTextField btnRoomType;
+    public JFXTextField btnQyt;
+    public JFXButton btnAddRoom;
+    public JFXTextField btnRoomTypeID;
+
+    public void btnRoomTypeIDOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnKeyMoneyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRoomTypeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnQytOnAction(ActionEvent actionEvent) {
+    }
 }
