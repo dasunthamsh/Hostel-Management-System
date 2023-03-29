11de89406f39c18f9aa4e80bea5f93f9c2290fd1
dasunthamsh <tunathamash@gmail.com>
2023-03-30 01:55:08 +0530

    added

--- src/lk/ijse/hostelManagementSystem/Controller/ManageStudentCFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/ManageStudentCFormController.java
@@ -2,8 +2,30 @@ package lk.ijse.hostelManagementSystem.Controller;/*
     @author Dasun
 */
 
+import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class ManageStudentCFormController {
     public AnchorPane pane;
+
+    public void btnStudentIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentNameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentAddressOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentNoOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBirthDayOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cmbGenderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddStudentNoOnAction(ActionEvent actionEvent) {
+    }
 }
