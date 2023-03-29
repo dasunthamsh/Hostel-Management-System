40e1ccfcf8ecdb4882c1c8f5559f08fbfd566ce9
dasunthamsh <tunathamash@gmail.com>
2023-03-30 01:55:49 +0530

    added

--- src/lk/ijse/hostelManagementSystem/Controller/ManageStudentCFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/ManageStudentCFormController.java
@@ -2,11 +2,22 @@ package lk.ijse.hostelManagementSystem.Controller;/*
     @author Dasun
 */
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class ManageStudentCFormController {
     public AnchorPane pane;
+    public JFXTextField btnStudentId;
+    public JFXTextField btnStudentName;
+    public JFXTextField btnStudentAddress;
+    public JFXTextField btnStudentNo;
+    public JFXDatePicker btnBirthDay;
+    public JFXComboBox cmbGender;
+    public JFXButton btnAddStudent;
 
     public void btnStudentIdOnAction(ActionEvent actionEvent) {
     }
