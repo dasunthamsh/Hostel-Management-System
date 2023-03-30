370cf3ebe8e4d016a6353e20ba619fad500c5eb0
dasunthamsh <tunathamash@gmail.com>
2023-03-30 18:55:26 +0530

    added

--- src/lk/ijse/hostelManagementSystem/util/FactoryConfiguration.java
+++ src/lk/ijse/hostelManagementSystem/util/FactoryConfiguration.java
@@ -4,6 +4,9 @@ package lk.ijse.hostelManagementSystem.util;/*
 
 import com.sun.deploy.security.SessionCertStore;
 import lk.ijse.hostelManagementSystem.Controller.DashboardFormController;
+import lk.ijse.hostelManagementSystem.Entity.Reservation;
+import lk.ijse.hostelManagementSystem.Entity.Room;
+import lk.ijse.hostelManagementSystem.Entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -15,7 +18,9 @@ public class FactoryConfiguration {
 
     private FactoryConfiguration(){
         sessionFactory = new Configuration().mergeProperties(Utility.getProperties())
-                .addAnnotatedClass(DashboardFormController.class)
+                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Room.class)
+                .addAnnotatedClass(Reservation.class)
                 .buildSessionFactory();
     }
 
