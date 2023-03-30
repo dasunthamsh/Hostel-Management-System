26806a1fdcc0187805d1f4445c9a0eb60ec2aa64
dasunthamsh <tunathamash@gmail.com>
2023-03-31 00:58:40 +0530

    added

--- src/lk/ijse/hostelManagementSystem/bo/impl/custom/BOFactory.java
+++ src/lk/ijse/hostelManagementSystem/bo/impl/custom/BOFactory.java
@@ -3,4 +3,16 @@ package lk.ijse.hostelManagementSystem.bo.impl.custom;/*
 */
 
 public class BOFactory {
+
+    private static BOFactory boFactory;
+
+    private BOFactory(){
+
+    }
+
+    public static BOFactory getInstance(){
+        return boFactory == null ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public
 }
