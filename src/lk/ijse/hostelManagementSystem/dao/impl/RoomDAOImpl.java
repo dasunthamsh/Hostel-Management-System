55e16ff031eac813fa4854dff768fce7714c2bb8
dasunthamsh <tunathamash@gmail.com>
2023-03-31 20:24:55 +0530

    added

--- src/lk/ijse/hostelManagementSystem/dao/impl/RoomDAOImpl.java
+++ src/lk/ijse/hostelManagementSystem/dao/impl/RoomDAOImpl.java
@@ -2,5 +2,27 @@ package lk.ijse.hostelManagementSystem.dao.impl;/*
     @author Dasun
 */
 
-public class RoomDAOImpl {
+import lk.ijse.hostelManagementSystem.Entity.Room;
+import lk.ijse.hostelManagementSystem.dao.RoomDAO;
+
+public class RoomDAOImpl implements RoomDAO {
+    @Override
+    public void save(Room entity) {
+
+    }
+
+    @Override
+    public void update(Room entity) {
+
+    }
+
+    @Override
+    public Room serch(String s) {
+        return null;
+    }
+
+    @Override
+    public void delete(String s) {
+
+    }
 }
