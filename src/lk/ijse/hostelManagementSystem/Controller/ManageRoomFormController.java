1e3a2b947434136989e0bb17b3f1d03cb3512b63
dasunthamsh <tunathamash@gmail.com>
2023-03-31 23:35:49 +0530

    added

--- src/lk/ijse/hostelManagementSystem/Controller/ManageRoomFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/ManageRoomFormController.java
@@ -43,6 +43,14 @@ public class ManageRoomFormController {
     }
 
     public void btnSearchRoomOnAction(ActionEvent actionEvent) {
+        RoomDTO room =roomBO.searcRoom(txtRoomTypeID.getText());
+
+
+
+        txtRoomTypeID.setText(room.getRoomTpeId());
+        cmbRoomType.setValue(room.getType());
+        txtQtq.setText(String.valueOf(room.getQyt()));
+        txtKeyMoney.setText(room.getKeyMoney());
 
     }
 
