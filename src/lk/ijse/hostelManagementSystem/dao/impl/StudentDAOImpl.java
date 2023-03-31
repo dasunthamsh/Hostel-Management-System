a6bfa7fbb9f94574a7393ec91262dcbfdbe5789c
dasunthamsh <tunathamash@gmail.com>
2023-03-31 15:55:58 +0530

    added

--- src/lk/ijse/hostelManagementSystem/dao/impl/StudentDAOImpl.java
+++ src/lk/ijse/hostelManagementSystem/dao/impl/StudentDAOImpl.java
@@ -2,7 +2,28 @@ package lk.ijse.hostelManagementSystem.dao.impl;/*
     @author Dasun
 */
 
+import lk.ijse.hostelManagementSystem.Entity.Student;
+import lk.ijse.hostelManagementSystem.dao.StudentDAO;
 import lk.ijse.hostelManagementSystem.dao.impl.custom.SuperDAO;
 
-public class StudentDAOImpl implements SuperDAO {
+public class StudentDAOImpl implements StudentDAO {
+    @Override
+    public void save(Student entity) {
+
+    }
+
+    @Override
+    public void update(Student entity) {
+
+    }
+
+    @Override
+    public Student serch(String s) {
+        return null;
+    }
+
+    @Override
+    public void delete(String s) {
+
+    }
 }
