40be8a8c0b5e1cc7a2562507de25b8c87c81e682
dasunthamsh <tunathamash@gmail.com>
2023-03-31 22:45:03 +0530

    added

--- src/lk/ijse/hostelManagementSystem/bo/impl/RoomBOImpl.java
+++ src/lk/ijse/hostelManagementSystem/bo/impl/RoomBOImpl.java
@@ -2,15 +2,20 @@ package lk.ijse.hostelManagementSystem.bo.impl;/*
     @author Dasun
 */
 
+import lk.ijse.hostelManagementSystem.Entity.Room;
 import lk.ijse.hostelManagementSystem.bo.RoomBO;
+import lk.ijse.hostelManagementSystem.dao.RoomDAO;
+import lk.ijse.hostelManagementSystem.dao.impl.custom.DAOFactory;
 import lk.ijse.hostelManagementSystem.dto.RoomDTO;
 
 
 public class RoomBOImpl implements RoomBO {
 
+        public RoomDAO roomDAO = (RoomDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.ROOM);
+
     @Override
     public void saveRoom(RoomDTO dto) {
-
+        roomDAO.save(new Room(dto.getRoomTpeId(),dto.getType(),dto.getKeyMoney(),dto.getQyt()));
     }
 
     @Override
