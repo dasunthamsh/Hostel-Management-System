c4979ec1bd2f230122dbf63b83450d689add171f
dasunthamsh <tunathamash@gmail.com>
2023-03-31 16:06:14 +0530

    added

--- src/lk/ijse/hostelManagementSystem/dao/impl/StudentDAOImpl.java
+++ src/lk/ijse/hostelManagementSystem/dao/impl/StudentDAOImpl.java
@@ -5,11 +5,20 @@ package lk.ijse.hostelManagementSystem.dao.impl;/*
 import lk.ijse.hostelManagementSystem.Entity.Student;
 import lk.ijse.hostelManagementSystem.dao.StudentDAO;
 import lk.ijse.hostelManagementSystem.dao.impl.custom.SuperDAO;
+import lk.ijse.hostelManagementSystem.util.FactoryConfigeration;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class StudentDAOImpl implements StudentDAO {
     @Override
     public void save(Student entity) {
 
+        Session session = FactoryConfigeration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(entity);
+        transaction.commit();
+        session.close();
     }
 
     @Override
