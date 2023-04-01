c2ccaa3e7ad714be7d83486653a1bda6b9401f0f
dasunthamsh <tunathamash@gmail.com>
2023-04-01 17:15:54 +0530

    added

--- src/lk/ijse/hostelManagementSystem/tm/StudentTM.java
+++ src/lk/ijse/hostelManagementSystem/tm/StudentTM.java
@@ -2,5 +2,21 @@ package lk.ijse.hostelManagementSystem.tm;/*
     @author Dasun
 */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentTM {
+
+    private String studentId;
+    private String name;
+    private String address;
+    private String contactNo;
+    private LocalDate dob;
+    private String gender;
 }
