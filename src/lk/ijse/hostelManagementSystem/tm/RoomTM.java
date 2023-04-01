f356cc859f07387cbd31af49519d99ecf0b20535
dasunthamsh <tunathamash@gmail.com>
2023-04-01 17:15:18 +0530

    added

--- src/lk/ijse/hostelManagementSystem/tm/RoomTM.java
+++ src/lk/ijse/hostelManagementSystem/tm/RoomTM.java
@@ -2,5 +2,18 @@ package lk.ijse.hostelManagementSystem.tm;/*
     @author Dasun
 */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class RoomTM {
+
+    private String roomTpeId;
+    private String type;
+    private String keyMoney;
+    private int qyt;
+
 }
