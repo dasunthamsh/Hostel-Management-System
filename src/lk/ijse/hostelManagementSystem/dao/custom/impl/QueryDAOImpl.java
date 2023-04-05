6281e575a114f9c338e33afdd71e4575356f404a
dasunthamsh <tunathamash@gmail.com>
2023-04-06 02:12:04 +0530

    join query added

--- src/lk/ijse/hostelManagementSystem/dao/custom/impl/QueryDAOImpl.java
+++ src/lk/ijse/hostelManagementSystem/dao/custom/impl/QueryDAOImpl.java
@@ -3,13 +3,22 @@ package lk.ijse.hostelManagementSystem.dao.custom.impl;/*
 */
 
 import lk.ijse.hostelManagementSystem.dao.custom.QueryDAO;
+import lk.ijse.hostelManagementSystem.util.FactoryConfigeration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
-public class QueryDAOImpl extends QueryDAO {
+public class QueryDAOImpl implements QueryDAO {
 
     @Override
     public List<Object[]> getPaymentStutas() {
-        return null;
+
+        Session session = FactoryConfigeration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        List<Object[]> list =session.createQuery("SELECT reservation.resId,s.studentId,s.name,r.roomTpeId , r.type , reservation.status FROM Reservation reservation JOIN Student s ON reservation.student=s.studentId JOIN Room r ON reservation.room=r.roomTpeId WHERE reservation.status='Pending'").list();
+        transaction.commit();
+        session.close();
+        return list;
     }
 }
