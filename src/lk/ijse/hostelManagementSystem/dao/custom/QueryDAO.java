4d950580f62ed91d04d1fe30034e746c4e7e7ca7
dasunthamsh <tunathamash@gmail.com>
2023-04-06 01:42:56 +0530

    queryDao method added

--- src/lk/ijse/hostelManagementSystem/dao/custom/QueryDAO.java
+++ src/lk/ijse/hostelManagementSystem/dao/custom/QueryDAO.java
@@ -2,5 +2,12 @@ package lk.ijse.hostelManagementSystem.dao.custom;/*
     @author Dasun
 */
 
-public interface QueryDAO {
+import lk.ijse.hostelManagementSystem.dao.SuperDAO;
+
+import java.util.List;
+
+public interface QueryDAO extends SuperDAO {
+
+    List<Object[]> getPaymentStutas();
+
 }
---
+++ src/lk/ijse/hostelManagementSystem/dao/custom/impl/QueryDAOImpl.java
@@ -0,0 +1,15 @@
+package lk.ijse.hostelManagementSystem.dao.custom.impl;/*
+    @author Dasun
+*/
+
+import lk.ijse.hostelManagementSystem.dao.custom.QueryDAO;
+
+import java.util.List;
+
+public class QueryDAOImpl extends QueryDAO {
+
+    @Override
+    public List<Object[]> getPaymentStutas() {
+        return null;
+    }
+}
