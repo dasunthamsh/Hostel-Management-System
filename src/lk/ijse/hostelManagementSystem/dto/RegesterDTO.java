f2091a408368503a11829c0841b308ca701c3a79
dasunthamsh <tunathamash@gmail.com>
2023-04-07 17:33:16 +0530

    RegesterDTO class

---
+++ src/lk/ijse/hostelManagementSystem/dto/RegesterDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.hostelManagementSystem.dto;/*
+    @author Dasun
+*/
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RegesterDTO {
+
+    private String id;
+    private String name;
+    private String email;
+    private String assword;
+
+
+}
