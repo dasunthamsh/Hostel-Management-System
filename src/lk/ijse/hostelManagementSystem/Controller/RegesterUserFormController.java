6a5b34896c64173d9a0a439e212aea70374583a3
dasunthamsh <tunathamash@gmail.com>
2023-04-07 17:31:13 +0530

    Regester Entity class

--- src/lk/ijse/hostelManagementSystem/Controller/RegesterUserFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/RegesterUserFormController.java
@@ -2,5 +2,23 @@ package lk.ijse.hostelManagementSystem.Controller;/*
     @author Dasun
 */
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class RegesterUserFormController {
+    public AnchorPane txtName;
+    public JFXTextField txtId;
+    public JFXTextField txtEmail1;
+    public JFXTextField txtEmail;
+    public JFXTextField txtPassword;
+    public JFXButton btnBack;
+    public JFXButton btnRegester;
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRegesterOnAction(ActionEvent actionEvent) {
+    }
 }
---
+++ src/lk/ijse/hostelManagementSystem/Entity/Regester.java
@@ -0,0 +1,22 @@
+package lk.ijse.hostelManagementSystem.Entity;/*
+    @author Dasun
+*/
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Regester {
+    @Id
+   private String id;
+   private String name;
+   private String email;
+   private String assword;
+}
