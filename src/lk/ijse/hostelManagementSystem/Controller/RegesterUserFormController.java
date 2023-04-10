7977a171a87d161473ad213bc7870da4095bc524
dasunthamsh <tunathamash@gmail.com>
2023-04-11 02:59:47 +0530

    room regex complate

--- src/lk/ijse/hostelManagementSystem/Controller/RegesterUserFormController.java
+++ src/lk/ijse/hostelManagementSystem/Controller/RegesterUserFormController.java
@@ -5,14 +5,19 @@ package lk.ijse.hostelManagementSystem.Controller;/*
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.hostelManagementSystem.bo.BOFactory;
 import lk.ijse.hostelManagementSystem.bo.custom.RegesterBO;
 import lk.ijse.hostelManagementSystem.dto.RegesterDTO;
 import lk.ijse.hostelManagementSystem.util.Navigation;
 import lk.ijse.hostelManagementSystem.util.Routes;
+import lk.ijse.hostelManagementSystem.util.ValidationUtil;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
 
 public class RegesterUserFormController {
 
@@ -25,6 +30,8 @@ public class RegesterUserFormController {
     public JFXTextField txtName;
 
     RegesterBO regesterBO = (RegesterBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.REGESTER);
+    private final LinkedHashMap<JFXTextField, Pattern> RegexMap = new LinkedHashMap<>();
+
 
     public void btnBackOnAction(ActionEvent actionEvent) throws IOException {
 
@@ -34,5 +41,16 @@ public class RegesterUserFormController {
     public void btnRegesterOnAction(ActionEvent actionEvent) throws IOException {
 
             regesterBO.saveUser(new RegesterDTO(txtId.getText(), txtName.getText(), txtEmail.getText(), txtPassword.getText()));
+    }
+
+    public void validateFieldsOnKeyRelease(KeyEvent keyEvent) {
+        Object validate = ValidationUtil.Validate(RegexMap, btnRegester);
+        if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+            if (validate instanceof JFXTextField) {
+                ((JFXTextField) validate).requestFocus();
+            } else {
+                btnRegester.fire();
+            }
+        }
     }
 }
